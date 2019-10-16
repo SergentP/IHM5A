798a93d9d78a78877e0c86b899c0d9f5b14e5794
SCHANEN Loic <loic.schanen@sfr.fr>
2019-10-16 11:14:38 +0000

    range slider view without interaction

--- RangeSlider/src/Main.java
+++ RangeSlider/src/Main.java
@@ -1,14 +1,48 @@
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
 
-public class Main {
-	
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				Window window = new Window();
-				window.setVisible(true);
-			}
-		});
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+
+	private static final int WINDOW_WIDTH = 250;
+	private static final int WINDOW_HEIGHT = 200;
+
+	public Main() {
+
+		this.setTitle("Range Slider");
+		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setLayout(new BorderLayout());
+		
+		Container c = this.getContentPane();
+
+		JPanel rangeSlider = createRangeSlider(0,100);
+
+		c.add(rangeSlider, BorderLayout.CENTER);
+
+		this.setVisible(true);
 	}
 
+	private JPanel createRangeSlider(int min, int max) {
+		
+		JPanel panel = new JPanel(new BorderLayout());
+		View rangeSlider = new View(min, max);
+		panel.add(rangeSlider, BorderLayout.CENTER);
+
+		return panel;
+	}
+
+	public static void main(String[] args) {
+
+		Main main = new Main();
+	}
 }
--- RangeSlider/src/View.java
+++ RangeSlider/src/View.java
@@ -1,36 +1,83 @@
-import java.awt.Rectangle;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 
-public class View extends JFrame {
+@SuppressWarnings("serial")
+public class View extends JComponent {
 
-	private static final int width = 10;
+	private int max_val;
+	private int min_val;
+	private int inc;
+
+	private static final int SLIDER_WIDTH = 200;
+	private static final int BUTTON_WIDTH = 25;
+	private static final int HEIGHT = 20;
+
+	private int min_x = BUTTON_WIDTH;
+	private int max_x = min_x + SLIDER_WIDTH;
+
+	private int left_button_x;
+	private int right_button_x;
+
+	public JLabel left_label;
+	public JLabel right_label;
 	
-	public View() {
+	Rectangle2D left_button;
+	Rectangle2D right_button;
+	Rectangle2D left_range;
+	Rectangle2D selected_range;
+	Rectangle2D right_range;
 
-		setTitle("Range Slider TP");
-		setSize(300, 200);
-		setResizable(false);
+	public View(int min, int max) {
 
-		JPanel pan = new JPanel();
-
-		addWindowListener(new WindowAdapter() {
-
-			@Override
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
+		min_val = min;
+		max_val = max;
 		
-		setVisible(true);
-	}
-	
-	
-	public static void main(String[] args) {
+		inc = SLIDER_WIDTH / (max_val - min_val);
 		
-		View view = new View();
+		left_button_x = min_x;
+		right_button_x = min_x + SLIDER_WIDTH;
+
+		left_label = new JLabel(Integer.toString(min));
+		right_label = new JLabel(Integer.toString(max));
+		
+		left_button = new Rectangle2D.Double(left_button_x - BUTTON_WIDTH / 2, HEIGHT, BUTTON_WIDTH, HEIGHT);
+		right_button = new Rectangle2D.Double(right_button_x - BUTTON_WIDTH / 2, HEIGHT, BUTTON_WIDTH, HEIGHT);
+		left_range = new Rectangle2D.Double(min_x, HEIGHT, left_button_x - BUTTON_WIDTH / 2 - min_x, HEIGHT);
+		selected_range = new Rectangle2D.Double(left_button_x + BUTTON_WIDTH / 2, HEIGHT, right_button_x - left_button_x - BUTTON_WIDTH, HEIGHT);
+		right_range = new Rectangle2D.Double(right_button_x + BUTTON_WIDTH / 2, HEIGHT, max_x - right_button_x - BUTTON_WIDTH / 2, HEIGHT);
+
 	}
+
+	public JLabel getLeftLabel() {
+		return left_label;
+	}
+
+	public JLabel getRightLabel() {
+		return right_label;
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setColor(Color.WHITE);
+		g2d.fill(left_button);
+		g2d.fill(right_button);
+		
+		g2d.setColor(Color.LIGHT_GRAY);
+		g2d.fill(left_range);
+		g2d.fill(right_range);
+
+		g2d.setColor(Color.GRAY);
+		g2d.fill(selected_range);
+	}
+
 }
