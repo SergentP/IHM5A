6cb5c4df494a66ccf37ab7e19fb3860af2eb643b
SCHANEN Loic <loic.schanen@sfr.fr>
2019-10-16 11:41:29 +0000

    Button can be dragged but no limits putsit needs to be linked to the model

--- RangeSlider/src/View.java
+++ RangeSlider/src/View.java
@@ -1,6 +1,10 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComponent;
@@ -45,12 +49,18 @@ public class View extends JComponent {
 		left_label = new JLabel(Integer.toString(min));
 		right_label = new JLabel(Integer.toString(max));
 		
+		createRectangle();
+		setListener();
+
+	}
+	
+	private void createRectangle() {
+		
 		left_button = new Rectangle2D.Double(left_button_x - BUTTON_WIDTH / 2, HEIGHT, BUTTON_WIDTH, HEIGHT);
 		right_button = new Rectangle2D.Double(right_button_x - BUTTON_WIDTH / 2, HEIGHT, BUTTON_WIDTH, HEIGHT);
 		left_range = new Rectangle2D.Double(min_x, HEIGHT, left_button_x - BUTTON_WIDTH / 2 - min_x, HEIGHT);
 		selected_range = new Rectangle2D.Double(left_button_x + BUTTON_WIDTH / 2, HEIGHT, right_button_x - left_button_x - BUTTON_WIDTH, HEIGHT);
 		right_range = new Rectangle2D.Double(right_button_x + BUTTON_WIDTH / 2, HEIGHT, max_x - right_button_x - BUTTON_WIDTH / 2, HEIGHT);
-
 	}
 
 	public JLabel getLeftLabel() {
@@ -60,7 +70,54 @@ public class View extends JComponent {
 	public JLabel getRightLabel() {
 		return right_label;
 	}
+	
+	private void setListener() {
+		
+		this.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				Point p = e.getPoint();
+				if (left_button.contains(p)) {
+					System.out.println("Left button clicked");
+				}
+				if (right_button.contains(p)) {
+					System.out.println("Right button clicked");
+				}
+				if (left_range.contains(p)) {
+					System.out.println("Left range clicked");
+				}
+				if (right_range.contains(p)) {
+					System.out.println("Right range clicked");
+				}
+				if (selected_range.contains(p)) {
+					System.out.println("Selected range clicked");
+				}
+			}
+			
+			public void mouseReleased(MouseEvent e) {
+				
+			}
+		});
+		
+		this.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				dragged(e);
+				
+			}
+		});
+	}
 
+	private void dragged(MouseEvent e) {
+		Point p = e.getPoint();
+		if (left_button.contains(p)) {
+			left_button_x = p.x;
+		}
+		if (right_button.contains(p)) {
+			right_button_x = p.x;
+		}
+		this.createRectangle();
+		this.repaint();
+	}
+	
 	@Override
 	public void paintComponent(Graphics g) {
 
