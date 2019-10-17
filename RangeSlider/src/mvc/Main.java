45bec203f1f4345bf2f9dd48654a09e5c5e80efe
SCHANEN Loic <loic.schanen@sfr.fr>
2019-10-17 23:15:37 +0000

    homefinder functional with better layout

--- RangeSlider/src/mvc/Main.java
+++ RangeSlider/src/mvc/Main.java
@@ -1,16 +1,16 @@
 package mvc;
 import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.Dimension;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Main {
 
-	private static final int WINDOW_WIDTH = 820;
-	private static final int WINDOW_HEIGHT = 600;
+	private static final int WINDOW_WIDTH = 1000;
+	private static final int WINDOW_HEIGHT = 650;
 
 	public static void affichage() {
 		JFrame frame = new JFrame();
@@ -22,23 +22,37 @@ public class Main {
 
 		frame.setLocationRelativeTo(null);
 		
-		frame.setLayout(new BorderLayout());
+		BorderLayout layout = new BorderLayout();
+		layout.setHgap(50);
+		frame.setLayout(layout);
 		
-		JPanel price = new View(0,99);
-		price.setPreferredSize(new Dimension(240,150));
-		JPanel room = new View(0, 10);
-		room.setPreferredSize(new Dimension(220,150));
-		price.setLayout(new BoxLayout(price, BoxLayout.PAGE_AXIS));
-
-		frame.getContentPane().add(price, BorderLayout.EAST);
+		JPanel sliders = new JPanel();
+		sliders.setLayout(new BoxLayout(sliders, BoxLayout.PAGE_AXIS));
 		
-		JPanel homefinder = new HomeFinder((View) price, (View) room, 10);
-		((View)price).control.addHF((HomeFinder)homefinder);
-		((View)room).control.addHF((HomeFinder)homefinder);
+		View price = new View(0,99);
+		price.setPreferredSize(new Dimension(300,150));
+		JLabel price_label = new JLabel("Price");
+		
+		sliders.add(price_label);
+		sliders.add(price);
+		
+		View room = new View(0, 10);
+		room.setPreferredSize(new Dimension(300,150));
+		JLabel room_label = new JLabel("Room number");
+		
+		sliders.add(room_label);
+		sliders.add(room);
+		
+		JPanel homefinder = new HomeFinder(price, room, 100);
+		price.control.addHF((HomeFinder)homefinder);
+		room.control.addHF((HomeFinder)homefinder);
 		homefinder.setPreferredSize(new Dimension(HomeFinder.X_AXIS, HomeFinder.Y_AXIS));
 		
+		frame.getContentPane().add(sliders, BorderLayout.EAST);
 		frame.getContentPane().add(homefinder, BorderLayout.CENTER);
 
+		
+		
 		frame.setVisible(true);
 	}
 	
